8ad7e84926def1ca75d79e07ff5ef15bb0678c14
theo <teddyhoover@gmail.com>
2021-03-02 10:25:43 -0600

    commit tree b47a95b57ec5971458c7d70322cd67f86da0038d

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+class ProductsController < ApplicationController
+    def index
+        
+    end
+
+    def add
+        #binding.pry
+        cart << params[:product]
+        redirect_to '/'
+    end
+end
